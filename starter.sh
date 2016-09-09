5ac1dfdf82577aeefeac66b3c8992c042c53032a
bluemagma612 <bryce@bluemagma.com>
2016-09-09 15:59:18 -0500

    updated starter with fb access token environment var

--- starter.sh
+++ starter.sh
@@ -4,5 +4,6 @@ if [ $(ps -e -o uid,cmd | grep $UID | grep node | grep -v grep | wc -l | tr -s "
 then
         export PATH=/usr/local/bin:$PATH
 	export NODE_ENV=production
+	export PAGE_ACCESS_TOKEN=EAARyZCsbsTZAMBANZCNUkLW2vGeIZCFDsfqYl6FqqV2rKrdnw6rAeOp9POzUXzPvX1uDqhPUvDlGBmPct6mwxXxu41vitFuDPLImmAr899m2880dXBPDtMMZB4n0lfBZC9hY7WLHmCcTBQZCy9OGEkPci8ZBKNCT4dhrm2UOcjCZBsgZDZD
 	cd /opt/3minuteherochatbot && forever --spinSleepTime 10000 start server.js >> forever.log 2>&1
 fi
